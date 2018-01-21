504bfa562352205a5b969204109d4bb5504d5fed
Lesuk <lesuk93@yahoo.com>
2018-01-21 17:16:59 +0200

    Webpack config: Move entry app path to commonConfig object

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ const isProd = TARGET_ENV == prod;
 
 // set entry and output path/filename
 const entryPath = path.join(__dirname, 'src/static/index.js');
+const entryPathDev = [ 'webpack-dev-server/client?http://localhost:8080', entryPath ]
 const outputPath = path.join(__dirname, 'dist');
 const outputFilename = isProd ? '[name]-[chunkhash].js' : '[name].js'
 
@@ -38,6 +39,7 @@ console.log('webpack is running...');
 var commonConfig = {
 
   entry: {
+    app: isProd ? entryPath : entryPathDev,
     vendor: [
       "./src/static/js/vendor/d3.custom.min",
       "./src/static/js/vendor/d3.layout.cloud.min",
@@ -142,16 +144,7 @@ if ( isDev ) {
   console.log('Development build');
 
   module.exports = merge( commonConfig, {
-
-    entry: {
-      app: [
-        'webpack-dev-server/client?http://localhost:8080',
-        entryPath
-      ]
-    },
-
     devServer: {
-      // Serve index.html in place of 404 responses,
       historyApiFallback: true,
       contentBase: './src',
       hot: true
@@ -164,11 +157,6 @@ if ( isProd ) {
   console.log('Production build');
 
   module.exports = merge( commonConfig, {
-
-    entry: {
-      app: entryPath
-    },
-
     plugins: [
       new CopyWebpackPlugin([
         {
